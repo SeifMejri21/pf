fba1fb36cfbeb514daa504e26cdfd4f5ae1b27ab
SeifMejri21 <58209253+SeifMejri21@users.noreply.github.com>
2024-01-20 12:00:45 +0100

    1st commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.utils.helpers import mean
+
+x = [1, 2, 3, 4, 5]
+
+y = mean(x)
+
+print(y)
---
+++ src/utils/helpers.py
@@ -0,0 +1,5 @@
+def mean(data_list):
+    my_sum = 0
+    for d in data_list:
+        my_sum += d
+    return my_sum / len(data_list)
